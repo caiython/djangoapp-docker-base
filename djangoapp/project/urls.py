22ff1da30c5c769897efebb1b3646bd1a7ab4d26
Caio Chagas <caiocvlopes@gmail.com>
2024-04-04 09:09:52 -0300

    urls do app incluidas nas configurações do projeto

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("app.urls")),
 ]
 
 if settings.DEBUG:
