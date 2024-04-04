a7ee92629877f3ac66828e5617dbbd5569120b37
Caio Chagas <caiocvlopes@gmail.com>
2024-04-04 19:52:23 -0300

    rota "celery_hello_world/" adicionada para testar a task assincrona e o worker

--- djangoapp/app/urls.py
+++ djangoapp/app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('celery_hello_world/', views.celery_hello_world, name='celery_hello_world')
 ]
--- djangoapp/app/views.py
+++ djangoapp/app/views.py
@@ -1,6 +1,13 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+
+from . import tasks
 
 
 # Create your views here.
 def index(request):
+    return JsonResponse({'Hello': 'world!'})
+
+
+def celery_hello_world(request: HttpResponse) -> JsonResponse:
+    tasks.hello_world.delay()
     return JsonResponse({'Hello': 'world!'})
