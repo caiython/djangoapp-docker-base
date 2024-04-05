a3f93c536d314431d017d4d63c1b1a14e7a7f040
Caio Chagas <caiocvlopes@gmail.com>
2024-04-05 00:57:19 -0300

    teste do selenium remoto criado

--- djangoapp/app/tests.py
+++ djangoapp/app/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from selenium import webdriver
 
 # Create your tests here.
+
+
+class SeleniumTest(TestCase):
+    fixtures = [
+        'app/fixtures/app.json',
+        ...
+    ]
+
+    def start(self):
+
+        selenium_grid_url = "http://selenium:4444/wd/hub"
+        options = webdriver.FirefoxOptions()
+
+        # Instantiate an instance of Remote WebDriver with the desired capabilities.
+        self.browser = webdriver.Remote(command_executor=selenium_grid_url,
+                                        keep_alive=True,
+                                        options=options)
+
+    def stop(self):
+        self.browser.quit()
+
+    def test_visit_site(self):
+        self.browser.get('http://djangoapp:8000/')
+        self.assertIn(self.browser.title, 'Home')
